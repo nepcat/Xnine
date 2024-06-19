a074a7464deee1fefe457aad5c3d456727b0ee5f
nepcat <153364073+nepcat@users.noreply.github.com>
2024-06-19 21:31:45 +0300

    Fix build script, update README for tf2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
-gcc -m32 -ldl -lX11 -lXext -lxcb -lxcb-present -lxcb-dri3 -lxcb-xfixes -lX11-xcb $(pkg-config --cflags --libs sdl2) -Iinclude/D3D9 -I /usr/include/d3dadapter SDL_nine.c dri3.c device_wrap.c -shared -fPIC   -g -o nine_sdl.so && \
-gcc -ldl -lX11 -lXext -lxcb -lxcb-present -lxcb-dri3 -lxcb-xfixes -lX11-xcb $(pkg-config --cflags --libs sdl2) -Iinclude/D3D9 -I /usr/include/d3dadapter SDL_nine.c dri3.c device_wrap.c -shared -fPIC   -g -o nine_sdl64.so
+gcc -m32 -ldl -lX11 -lXext -lxcb -lxcb-present -lxcb-dri3 -lxcb-xfixes -lX11-xcb $(pkg-config --cflags --libs sdl2) -Iinclude/D3D9 -I /usr/include/d3dadapter SDL_nine.c dri3.c device_wrap.c -shared -fPIC   -g -o nine_sdl.so -Wno-incompatible-pointer-types && \
+gcc -ldl -lX11 -lXext -lxcb -lxcb-present -lxcb-dri3 -lxcb-xfixes -lX11-xcb $(pkg-config --cflags --libs sdl2) -Iinclude/D3D9 -I /usr/include/d3dadapter SDL_nine.c dri3.c device_wrap.c -shared -fPIC   -g -o nine_sdl64.so -Wno-incompatible-pointer-types
